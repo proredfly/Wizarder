31bee7a088c60e5bc36fef8e99d61f819636e29b
CAMDEN HARTSELL <51464@rwisd.int>
2023-11-29 15:32:48 -0600

    mostly minor changes ngl

--- Assets/Scripts/Hold.cs
+++ Assets/Scripts/Hold.cs
@@ -1,6 +1,9 @@
+using StarterAssets;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEditor.SceneTemplate;
 using UnityEngine;
 
 public class Hold : MonoBehaviour
@@ -10,17 +13,41 @@ public class Hold : MonoBehaviour
     public int holdNum;
     public Color startColor, endColor;
     public float startTime;
+    public GameObject _mainCamera;
     Boolean inProgress = false;
 
+    private void Start()
+    {
+        _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
     public void FixedUpdate()
     {
-        if(startColor != endColor && !inProgress)
+        
+        // enable particle system on hold if close enough
+        if(Vector3.Distance(transform.position, _mainCamera.transform.position) <= 5)
+        {
+            var eTemp = GetComponentInChildren<ParticleSystem>().emission;
+            eTemp.enabled = true;
+        } else
+        {
+            var eTemp = GetComponentInChildren<ParticleSystem>().emission;
+            eTemp.enabled = false;
+            GetComponentInChildren<ParticleSystem>().Clear();
+        }
+       
+        // when end color is different it runs changeColor method
+        // in progress to make sure it doesnt run when 
+        if (startColor != endColor && !inProgress)
         {
             inProgress = true;
+            // create coroutine to change color with fixed update
             StartCoroutine(ChangeColor());
             startTime = Time.time;
         } 
     }
+
+    // change color over time 
+    // generally ran as coroutine with fixed update
     public IEnumerator ChangeColor()
     {
         float tick = 0f;
@@ -28,9 +55,10 @@ public class Hold : MonoBehaviour
         {
             tick += Time.deltaTime * speed;
             r.material.color = Color.Lerp(startColor, endColor, tick);
-            Debug.Log("attempting to work");
             yield return null;
         }
+
+        //reset and go back to white with the same method
         Color temp = endColor;
         endColor = startColor;
         startColor = temp;
@@ -40,7 +68,6 @@ public class Hold : MonoBehaviour
         {
             tick += Time.deltaTime * speed;
             r.material.color = Color.Lerp(startColor, endColor, tick);
-            Debug.Log("attempting to work");
             yield return null;
         }
         startColor = endColor;
